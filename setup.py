6490be39e20323d42c9b57920a4f8751e2966d5d
Hervé BREDIN <bredin@limsi.fr>
2016-09-22 09:02:10 +0200

    chore: update to pyannote.database 0.3

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     },
     include_package_data=True,
     install_requires=[
-        'pyannote.database >= 0.2',
+        'pyannote.database >= 0.3',
         'pyannote.parser >= 0.4.2',
     ],
     classifiers=[
